e280b371f188ecf451b58554ac6ad9681b327a7f
yashpitrod <165854051+yashpitrod@users.noreply.github.com>
2025-09-08 18:27:04 +0530

    Update app.py
    app.py changes

--- Hackodisha/app.py
+++ Hackodisha/app.py
@@ -710,4 +710,5 @@ if __name__ == '__main__':
     print("🌐 Server starting on http://localhost:5000")
     print("=" * 60)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+        port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
